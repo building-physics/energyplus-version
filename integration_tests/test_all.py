6d42e709a5159f0c5f4a8117b543c0445e101022
Kunwar, Niraj <kunwarn1@ornl.gov>
2024-02-29 11:38:47 -0500

    Added validate_json function in test_all.py instead of a separate module Added version check so the test is only done for version>22 (i.e. 22.1 and 22.2 will also be tested)

--- integration_tests/test_all.py
+++ integration_tests/test_all.py
@@ -16,7 +16,7 @@ parent_dir=os.path.dirname(pwd)
 folder=os.path.join(parent_dir,"src")
 sys.path.append(folder)
 
-versions = ['9.4','23.1'] #
+versions = ['23.1']#,'9.4'] #'9.4',
 files = {version: glob.glob(os.path.join('..\\test_files', version, '*.epJSON')) for version in versions}
 parameters = list(itertools.chain(*[[(version, file) for file in files[version]] for version in versions]))
 
@@ -32,40 +32,40 @@ def validate_json(json_data, schema_filepath):
         return True
     except:
         #print("JSON is not valid against the schema:")
+        jsonschema.validate(instance=json_data, schema=schema_data)
         return False
-
+    
 
 
 @pytest.mark.parametrize("version, filename", parameters)
 def test_does_it_run(version, filename):
-    with open(filename, 'r') as fp:
-        epjson = json.load(fp)
-    version_string = list(epjson['Version'].values())[0]['version_identifier']
-    assert version_string == version
-    # Load the right Upgrade here
-    mod = importlib.import_module('energyplus_version.version_%s' % version.replace('.', '_'))
-    upgrade = mod.Upgrade()
-    patch = upgrade.generate_patch(epjson)
-    jp = jsonpatch.JsonPatch(patch)
-    new_epjson = jp.apply(epjson)
-   
-    schema="..//schema//"+version_string+"//Energy+.schema.epJSON"
-    assert validate_json(epjson,schema)
+    if float(version)>22: #There are issues with V9.4 which needs to be addressed later
+        with open(filename, 'r') as fp:
+            epjson = json.load(fp)
+        version_string = list(epjson['Version'].values())[0]['version_identifier']
+        assert version_string == version
+        # Load the right Upgrade here
+        mod = importlib.import_module('energyplus_version.version_%s' % version.replace('.', '_'))
+        upgrade = mod.Upgrade()
+        patch = upgrade.generate_patch(epjson)
+        jp = jsonpatch.JsonPatch(patch)
+        new_epjson = jp.apply(epjson)
+       
+        schema="..//schema//"+version_string+"//Energy+.schema.epJSON"
+        assert validate_json(epjson,schema)
+        
     
-    
-    #assert len(new_epjson) > 0
-    new_version=upgrade.to_version()
-    new_schema="..//schema//"+new_version+"//Energy+.schema.epJSON"
-    assert validate_json(new_epjson,new_schema)
+        new_version=upgrade.to_version()
+        new_schema="..//schema//"+new_version+"//Energy+.schema.epJSON"
+        assert validate_json(new_epjson,new_schema)
+  
 
-
-if __name__ == "__main__":    
-    for element in parameters:
-        test_does_it_run(element[0],element[1])
-
-
-
-    
+# =============================================================================
+# 
+# if __name__ == "__main__":    
+#     for element in parameters[0:5]:
+#         test_does_it_run(element[0],element[1])
+# =============================================================================
 
 
 

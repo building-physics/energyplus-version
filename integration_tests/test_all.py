55adbd42c025c7da68bc990031f8236571b56a7e
Jason W. DeGraw <jason.degraw@gmail.com>
2024-03-05 02:31:49 -0500

    Clean up some comments, refactor test

--- integration_tests/test_all.py
+++ integration_tests/test_all.py
@@ -9,63 +9,41 @@ import itertools
 import json
 import jsonschema
 import importlib
-import sys
 
-pwd=os.getcwd()
-parent_dir=os.path.dirname(pwd)
-folder=os.path.join(parent_dir,"src")
-sys.path.append(folder)
-
-versions = ['23.1']#,'9.4'] #'9.4',
-files = {version: glob.glob(os.path.join('..\\test_files', version, '*.epJSON')) for version in versions}
+versions = ['9.4', '23.1']
+files = {version: glob.glob(os.path.join('test_files', version, '*.epJSON')) for version in versions}
 parameters = list(itertools.chain(*[[(version, file) for file in files[version]] for version in versions]))
 
-
-
-def validate_json(json_data, schema_filepath):
-    
+# This should probably be moved into a fixture at some point
+def load_schema(version):
+    schema_filepath = os.path.join('schema', version, 'Energy+.schema.epJSON')
     with open(schema_filepath) as schema_file:
-        schema_data=json.load(schema_file) 
-    try:
-        jsonschema.validate(instance=json_data, schema=schema_data)
-        #print("JSON is valid against the schema.")
-        return True
-    except:
-        #print("JSON is not valid against the schema:")
-        jsonschema.validate(instance=json_data, schema=schema_data)
-        return False
+        schema_data=json.load(schema_file)
+    return schema_data 
     
+schemas = {version: load_schema(version) for version in ['9.4', '23.1', '23.2']}
 
+def validate_json(json_data, version):
+    try:
+        jsonschema.validate(instance=json_data, schema=schemas[version])
+        return True
+    except jsonschema.exceptions.ValidationError:
+        return False
 
 @pytest.mark.parametrize("version, filename", parameters)
 def test_does_it_run(version, filename):
+    with open(filename, 'r') as fp:
+        epjson = json.load(fp)
+    version_string = list(epjson['Version'].values())[0]['version_identifier']
+    assert version_string == version
+    # Load the right Upgrade here
+    mod = importlib.import_module('energyplus_version.version_%s' % version.replace('.', '_'))
+    upgrade = mod.Upgrade()
+    patch = upgrade.generate_patch(epjson)
+    jp = jsonpatch.JsonPatch(patch)
+    new_epjson = jp.apply(epjson)
+    assert new_epjson
     if float(version)>22: #There are issues with V9.4 which needs to be addressed later
-        with open(filename, 'r') as fp:
-            epjson = json.load(fp)
-        version_string = list(epjson['Version'].values())[0]['version_identifier']
-        assert version_string == version
-        # Load the right Upgrade here
-        mod = importlib.import_module('energyplus_version.version_%s' % version.replace('.', '_'))
-        upgrade = mod.Upgrade()
-        patch = upgrade.generate_patch(epjson)
-        jp = jsonpatch.JsonPatch(patch)
-        new_epjson = jp.apply(epjson)
-       
-        schema="..//schema//"+version_string+"//Energy+.schema.epJSON"
-        assert validate_json(epjson,schema)
-        
-    
+        # Check that the new data is valid
         new_version=upgrade.to_version()
-        new_schema="..//schema//"+new_version+"//Energy+.schema.epJSON"
-        assert validate_json(new_epjson,new_schema)
-  
-
-# =============================================================================
-# 
-# if __name__ == "__main__":    
-#     for element in parameters[0:5]:
-#         test_does_it_run(element[0],element[1])
-# =============================================================================
-
-
-
+        assert validate_json(new_epjson,new_version)

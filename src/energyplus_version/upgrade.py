06004b1e95c8745e64b3aa9b01e3ddc1ed190dff
Jason W. DeGraw <jason.degraw@gmail.com>
2024-03-25 16:37:54 -0400

    Fix one last thing (hopefully)

--- src/energyplus_version/upgrade.py
+++ src/energyplus_version/upgrade.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Oak Ridge National Laboratory, managed by UT-Battelle
 #
 # SPDX-License-Identifier: BSD-3-Clause
-from typing import Callable
+#from typing import Callable
 
 class UpgradeError(Exception):
     pass

0ae8189cfdddeeb2287f7fcc7f217c4defe930ef
Jason W. DeGraw <jason.degraw@gmail.com>
2024-03-27 15:58:45 -0400

    Fix failing unit tests

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -5,11 +5,11 @@ import energyplus_version
 
 def test_version():
     version = energyplus_version.EnergyPlusVersion(22, 2)
-    assert str(version) == '22.2'
-    assert str(version.previous()) == '22.1'
-    assert str(version.next()) == '23.1'
-    assert str(version.next().next()) == '23.2'
-    assert str(version.next().previous()) == '22.2'
+    assert str(version) == '22.2.0'
+    assert str(version.previous()) == '22.1.0'
+    assert str(version.next()) == '23.1.0'
+    assert str(version.next().next()) == '23.2.0'
+    assert str(version.next().previous()) == '22.2.0'
 
 def test_errors():
     version = energyplus_version.EnergyPlusVersion.from_string('22.one')
@@ -17,9 +17,9 @@ def test_errors():
 
 def test_version_string():
     version = energyplus_version.EnergyPlusVersion.from_string('22.2')
-    assert str(version) == '22.2'
-    assert str(version.previous()) == '22.1'
-    assert str(version.next()) == '23.1'
-    assert str(version.next().next()) == '23.2'
-    assert str(version.next().previous()) == '22.2'
+    assert str(version) == '22.2.0'
+    assert str(version.previous()) == '22.1.0'
+    assert str(version.next()) == '23.1.0'
+    assert str(version.next().next()) == '23.2.0'
+    assert str(version.next().previous()) == '22.2.0'
     
